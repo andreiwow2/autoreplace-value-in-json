af8338495a008ddcc696946c79b202e7c3560349
Divergent <andreigames9@gmail.com>
2023-01-11 20:27:29 +0100

    first commit

---
+++ autoreplacenbname.ps1
@@ -0,0 +1,46 @@
+function Get-FolderName {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
+        [string]$Message = "Select a directory.",
+
+        [string]$InitialDirectory = [System.Environment+SpecialFolder]::MyComputer,
+
+        [switch]$ShowNewFolderButton
+    )
+
+    $browserForFolderOptions = 0x00000041                                  # BIF_RETURNONLYFSDIRS -bor BIF_NEWDIALOGSTYLE
+    if (!$ShowNewFolderButton) { $browserForFolderOptions += 0x00000200 }  # BIF_NONEWFOLDERBUTTON
+
+    $browser = New-Object -ComObject Shell.Application
+    # To make the dialog topmost, you need to supply the Window handle of the current process
+    [intPtr]$handle = [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle
+
+    # see: https://msdn.microsoft.com/en-us/library/windows/desktop/bb773205(v=vs.85).aspx
+    $folder = $browser.BrowseForFolder($handle, $Message, $browserForFolderOptions, $InitialDirectory)
+
+    $result = $null
+    if ($folder) { 
+        $result = $folder.Self.Path 
+    } 
+
+    # Release and remove the used Com object from memory
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($browser) | Out-Null
+    [System.GC]::Collect()
+    [System.GC]::WaitForPendingFinalizers()
+
+
+    return $result
+}
+
+$folder = Get-FolderName
+if ($folder) { 
+    Get-ChildItem -Path $folder -Filter *.json -Recurse -File | ForEach-Object {
+        $filename = $_.FullName
+        $a = Get-Content $filename -raw | ConvertFrom-Json
+        $a.geraeteBezeichnung=77
+        $a | ConvertTo-Json -Depth 100 | Out-File $filename -Encoding UTF8
+        Write-Host "File $($filename).json was changed."
+    }
+ }
+else { "You did not select a directory." }
